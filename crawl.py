367c000da28841f4ac4641a4f97e9a3e8fbf6813
Akash Rotti <rottiakash@gmail.com>
2020-09-18 19:01:55 +0530

    semantics

--- crawl.py
+++ crawl.py
@@ -9,22 +9,25 @@ cred = credentials.Certificate(
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 collection = db.collection('recipies')
-while True:
-    URL = input("Enter URL")
-    r = requests.get(URL)
-    soup = BeautifulSoup(r.content, 'html5lib')
-    title = soup.find('h1', attrs={'class': 'entry-title'}).text
-    require = (list(map(lambda x: x.text, soup.findAll(
-        'span', attrs={'class': 'wprm-recipe-ingredient-name'}))))
-    obj = {
-        "name": title,
-        "url": URL,
-        "require": require
-    }
-    print(json.dumps(obj, indent=4))
-    choice = input("Write to Firestore: Y/N")
-    if choice == "Y" or choice == "y":
-        collection.add(obj)
-        print("Added to Firestore")
-    print("Keyboard Intrupt to stop")
-    print("===================================")
+try:
+    while True:
+        URL = input("Enter URL: ")
+        r = requests.get(URL)
+        soup = BeautifulSoup(r.content, 'html5lib')
+        title = soup.find('h1', attrs={'class': 'entry-title'}).text
+        require = (list(map(lambda x: x.text, soup.findAll(
+            'span', attrs={'class': 'wprm-recipe-ingredient-name'}))))
+        obj = {
+            "name": title,
+            "url": URL,
+            "require": require
+        }
+        print(json.dumps(obj, indent=4))
+        choice = input("Write to Firestore: Y/N: ")
+        if choice == "Y" or choice == "y":
+            collection.add(obj)
+            print("Added to Firestore")
+        print("Keyboard Intrupt to stop")
+        print("===================================")
+except:
+    print("\nDone")
